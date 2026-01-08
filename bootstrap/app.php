0b3c103f3679121ca55fc5d434c11dd15b37272b
m6rc0sp <paulo.marcos0108@live.com>
2026-01-08 17:11:55 -0300

    feat: adiciona suporte para geração de URLs com HTTPS e registra TrustProxies no middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Registrar TrustProxies para reconhecer X-Forwarded-* do Apache
+        $middleware->trustProxies(at: '*');
+
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
